9bfa41d0ad61c4787d1a216005ed6464b9d71831
emay007 <87287492+emay007@users.noreply.github.com>
2021-08-02 18:58:01 -0800

    Add files via upload

---
+++ review/jamaBang.cpp
@@ -0,0 +1,35 @@
+    import java.util.*;
+	class JamaBang{
+	   public static void main (String []args){
+	      scanner sc = new Scanner (System.in);
+		  char p1,p2,p3,p4;
+		  char q1,q2,q3,q4;
+          
+		  
+		  System.out.print("Enter first p: ");
+		  p1 = sc.nextLine().charAt(0);
+		  System.out.print("Enter second p: ");
+		  p2 = sc.nextLine().charAt(0);
+		  System.out.print("Enter third p: ");
+		  p3 = sc.nextLine().charAt(0);
+		  System.out.print("Enter fourth p: ");
+		  p4 = sc,nextLine().charAt(0);
+		  System.out.print("Enter first q: ");
+		  q1 = sc.nextLine().charAt(0);
+		  System.out.print("Enter second q:");
+		  q2 = sc.nextLine().charAt(0);
+		  System.out.print("Enter third q: ");
+		  q3 = sc.nextLine() charAt(0);
+		  System.out.print("Enter fourth q:");
+		  q4 = sc.nextLine().charAt(0);
+		  
+		  
+		  System.out.println("p\tq\tp||q");
+		   System.out.print(p1+"\t"+q1+"\t");
+		   
+		 
+		  
+		  
+		  
+		  
+
